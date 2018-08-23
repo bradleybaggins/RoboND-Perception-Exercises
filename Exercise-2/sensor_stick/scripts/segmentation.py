5d3a23dbf07ec0c854b6d12df9e795796a5aeab3
brad <bradbolliger@gmail.com>
2018-08-22 20:42:38 -0500

    Update segmentation.py

--- Exercise-2/sensor_stick/scripts/segmentation.py
+++ Exercise-2/sensor_stick/scripts/segmentation.py
@@ -11,13 +11,56 @@ def pcl_callback(pcl_msg):
     # TODO: Convert ROS msg to PCL data
 
     # TODO: Voxel Grid Downsampling
-
+    vox = cloud.make_voxel_grid_filter()
+    # choose voxel (aka leaf) size
+    LEAF_SIZE = .01
+    # set the voxel/leaf size
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+    # call .filter() to obtain the downsampled point cloud
+    cloud_filtered = vox.filter()
+    filename = 'voxel_downsampled.pcd'
+    pcl.save(cloud_filtered, filename)
+    
     # TODO: PassThrough Filter
-
+    passthrough = cloud_filtered.make_passthrough_filter()
+    # assign axis and range
+    filter_axis = 'z'
+    passthrough.set_fiter_field_name(filter_axis)
+    axis_min = 0.6
+    axis_max = 1.1
+    passthrough.set_filter_limits(axis_min, axis_max)
+    # call .filter() to obtain filtered point cloud
+    cloud_filtered = passthrough.filter()
+    filename = 'pass_through_filtered.pcd'
+    pcl.save(cloud_filtered, filename)
 
     # TODO: RANSAC Plane Segmentation
+    seg = cloud_filtered.make_segmenter()
+    # set model
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    # max distance threshold for consideration
+    max_distance = .01
+    seg.set_distance_threshold(max_distance)
+    # call .segment() to obtain set of inlier indices on d model coefficients
+    inliers, coefficients = seg.segment()
 
     # TODO: Extract inliers and outliers
+    extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+    filename = 'extracted_inliers.pcd'
+    pcl.save(extracted_inliers, filename)
+    extracted_outliers = cloud_filtered.extract(inliers, negative=True)
+    filename = 'extracted_outliers.pcd'
+    pcl.save(extracted_outliers, filename)
+    outlier_filter = cloud_filtered.make_statistical_outlier_filter()
+    # set number of neighboring points to analyze
+    outlier_filter.set_mean_k(50)
+    # set threshold scale factor
+    x = 1.0
+    # outlier is any point with a mean distance larger than global (mean distance + x*std_dev)
+    outlier_filter.set_std_dev_mul_thresh(x)
+    # call .filter()
+    cloud_filtered = outlier_filter.filter()
 
     # TODO: Euclidean Clustering
 
@@ -26,17 +69,22 @@ def pcl_callback(pcl_msg):
     # TODO: Convert PCL data to ROS messages
 
     # TODO: Publish ROS messages
-
+    pcl_objects_pub.publish(pcl_msg)
+    pcl_table_pub.publish(pcl_msg)
 
 if __name__ == '__main__':
 
     # TODO: ROS node initialization
-
+    rospy.init_node('clustering', anonymous=True)
     # TODO: Create Subscribers
-
+    pcl_sub = rospy.Subscriber("/sensor_stick/point_cloud"), pc2.PointCloud2, pcl_callback, queue_size=1)
     # TODO: Create Publishers
-
+    pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
+    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
     # Initialize color_list
     get_color_list.color_list = []
 
     # TODO: Spin while node is not shutdown
+    while not rospy.is_shutdown():
+        rospy.spin()
+    
