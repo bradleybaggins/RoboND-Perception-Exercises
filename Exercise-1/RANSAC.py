eae7ca4e7142dc166f599b34bf0655ec6d6000b2
brad <bradbolliger@gmail.com>
2018-08-22 19:30:11 -0500

    Update RANSAC.py

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -24,13 +24,21 @@ passthrough.set_fiter_field_name(filter_axis)
 axis_min = 0.6
 axis_max = 1.1
 passthrough.set_filter_limits(axis_min, axis_max)
-#call .filter() to obtain filtered point cloud
+# call .filter() to obtain filtered point cloud
 cloud_filtered = passthrough.filter()
 filename = 'pass_through_filtered.pcd'
 pcl.save(cloud_filtered, filename)
 
 # RANSAC plane segmentation
-
+seg = cloud_filtered.make_segmenter()
+# set model
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+#max distance threshold for consideration
+max_distance = 1
+seg.set_distance_threshold(max_distance)
+# call .segment() to obtain set of inlier indices on d model coefficients
+inliers, coefficients = seg.segment()
 
 # Extract inliers
 
